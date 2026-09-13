b4b84de5a3e94be6185ad7b6588a3b8fe78e2d7e
duongwthuj <thunguyenduong37@gmail.com>
2026-09-13 11:02:32 +0700

    feat(parent): add data models and API client
    - Add ChildScheduleModel, ChildCourseModel, ChildAssignmentModel
    - Add ParentApiClient with endpoints for schedule, courses, assignments
    - Update models.dart exports

--- lib/features/parent/data/models/models.dart
+++ lib/features/parent/data/models/models.dart
@@ -1,3 +1,6 @@
+export 'child_assignment_model.dart';
+export 'child_course_model.dart';
 export 'child_model.dart';
 export 'child_overview_model.dart';
+export 'child_schedule_model.dart';
 export 'parent_alert_model.dart';
